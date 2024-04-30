66ad4930a2347bb4da31320441485bf6fb42d0a2
Carlos Edu <carloseduardoec2004@gmail.com>
2024-04-30 10:02:10 -0300

    nono projeto

--- projeto.py
+++ projeto.py
@@ -1,1 +1,10 @@
-print("Commit inicial mudei2")
+import os
+os.system('cls')
+
+lista = ['carlos', 'mateus', 'amanda', 'malu', 'joao']
+
+for nome in lista:
+    if nome != 'joao':
+        print(f"{nome} Aceito")
+    else:
+        print(f"{nome} Negado")
