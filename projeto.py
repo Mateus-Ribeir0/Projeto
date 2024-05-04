0a27a0fe10495fe73dd3913b1942d34b3a2bb7c3
Carlos Edu <carloseduardoec2004@gmail.com>
2024-05-04 13:52:09 -0300

    Menu interativo V0

--- projeto.py
+++ projeto.py
@@ -1,9 +1,59 @@
 import os
 os.system('cls')
 
-file = open("Repositorio_de_receitas.txt", "r", encoding="utf8")
+def menu_interativo(acao):
+    if acao == 'A':
+        return adicionar()
+    elif acao == 'V':
+        return visualizar()
+    elif acao == 'R':
+        return atualizar()
+    elif acao == 'E':
+        return excluir()
+    elif acao == 'F':
+        return filtragem()
+    else:
+        return 'Ação inválida'
+            
+def adicionar():
 
 
+    return 'teste1'
 
-print(file.read())
+def visualizar():
+
+
+    return 'teste2'
+
+def atualizar():
+
+
+    return 'teste3'
+
+def excluir():
+
+
+    return 'teste4'
+
+def filtragem():
+
+
+    return 'teste5'
+
+#============ MENU =======================#
+print("======{ Menu do Programa }======\n")
+print("A - Adicionar receita")
+print("V - Visualizar receitas ")
+print("R - Atualizar receitas")
+print("E - Excluir receita")
+print("F - Filtragem por País")
+acao = str(input("\nAção: ").upper())
+#=========================================#
+
+print(menu_interativo(acao))
+
+
+        
+
+
 
