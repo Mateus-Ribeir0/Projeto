fff5afbf74dae98e9a60586da80184e0c440d236
Carlos Edu <carloseduardoec2004@gmail.com>
2024-05-04 16:32:44 -0300

    Função de cadastros de receitas + Menu V1

--- projeto.py
+++ projeto.py
@@ -2,8 +2,8 @@ import os
 os.system('cls')
 
 def menu_interativo(acao):
-    if acao == 'A':
-        return adicionar()
+    if acao == 'C':
+        return cadastar_receita()
     elif acao == 'V':
         return visualizar()
     elif acao == 'R':
@@ -12,13 +12,47 @@ def menu_interativo(acao):
         return excluir()
     elif acao == 'F':
         return filtragem()
+    elif acao == 'S':
+        return 'PROGRAMA ENCERRADO'
     else:
         return 'Ação inválida'
             
-def adicionar():
+def cadastar_receita():
+    os.system('cls')
+    file = open('Repositorio_de_receitas.txt', 'a', encoding='utf8')
 
+    formatador = []
+    print("   Cadastro de receitas   ")
+    print("==========================")
+    nome = str(input("Nome do prato: "))
+    formatador.append(nome)
 
-    return 'teste1'
+    print("==========================")
+    pais = str(input("País de origem: "))
+    formatador.append(pais)
+
+    print("==========================")
+    i = []
+    quant_i = int(input("Quantidade de ingredientes: "))
+    for j in range (quant_i):
+        ingrediente = str(input(f"Digite o {j+1}° Ingrediente: ").strip())
+        i.append(ingrediente)
+    i1 = "|".join(i)
+    formatador.append(i1)
+
+    print("==========================")
+    m = []
+    quant_m = int(input("Quantidade de passos: "))
+    for i in range (quant_m):
+        modo = str(input(f"Digite o {i+1}° passo: ").strip())
+        m.append(modo)
+    m1 = "|".join(m)
+    formatador.append(m1)
+
+    nova_receita = '\n' + ' - '.join(formatador) + '-False'
+    file.write(nova_receita)
+
+    file.close()
 
 def visualizar():
 
@@ -40,20 +74,22 @@ def filtragem():
 
     return 'teste5'
 
-#============ MENU =======================#
-print("======{ Menu do Programa }======\n")
-print("A - Adicionar receita")
-print("V - Visualizar receitas ")
-print("R - Atualizar receitas")
-print("E - Excluir receita")
-print("F - Filtragem por País")
-acao = str(input("\nAção: ").upper())
+#============ MENU PRINCIPAL ============#
+
+while True:
+    os.system('cls')
+    print("======{ Menu de receitas }======\n")
+    print("C - Cadastrar receitas")
+    print("V - Visualizar receitas ")
+    print("A - Atualizar receitas")
+    print("E - Excluir receita")
+    print("F - Filtragem por País")
+    print("S - Sair do programa")
+    acao = str(input("\nAção: ").upper())
+
+    print(menu_interativo(acao)) # roda/printa a função escolhida
+
+    if acao == 'S':
+        break
+    
 #=========================================#
-
-print(menu_interativo(acao))
-
-
-        
-
-
-
